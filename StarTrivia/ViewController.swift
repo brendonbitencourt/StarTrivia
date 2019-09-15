f69d759ebc7a4df3b284c05faf198999fc0c8f66
Brendon Bitencourt Braga <brendon.braga@gmail.com>
2019-09-14 22:23:16 -0300

    Initial Commit

---
+++ StarTrivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarTrivia
+//
+//  Created by Brendon Bitencourt Braga on 2019-09-14.
+//  Copyright © 2019 Brendon Bitencourt Braga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
