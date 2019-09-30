f8c935a0cd9c9d53f1349842caea5a9ea7d9096b
Brendon Bitencourt Braga <brendon.braga@gmail.com>
2019-09-29 21:24:30 -0300

    News ViewController Files

---
+++ StarTrivia/Controller/StarshipsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StarshipsViewController.swift
+//  StarTrivia
+//
+//  Created by Brendon Bitencourt Braga on 2019-09-29.
+//  Copyright © 2019 Brendon Bitencourt Braga. All rights reserved.
+//
+
+import UIKit
+
+class StarshipsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
